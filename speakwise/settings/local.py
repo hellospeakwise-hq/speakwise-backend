93a795ef87d185da7e22622a3ab01160bd4ebc9f
Ezi-code <ezrayendau2000@hotmail.com>
2025-08-19 15:32:57 +0000

    update database config

--- speakwise/settings/local.py
+++ speakwise/settings/local.py
@@ -45,7 +45,14 @@ CORS_ALLOW_HEADERS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default":{os.getenv("DATABASE_URL", default="sqlite:///db.sqlite3"),}
+    "default":{
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DB_PORT")
+    }
 }
 
 # Static files (CSS, JavaScript, Images)

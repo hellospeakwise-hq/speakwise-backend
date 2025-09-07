1d45a91413c8247084b1c504a8f4bb8e5dcf841b
ezi-code <ezrayendau2000@hotmail.com>
2025-09-07 22:02:41 +0000

    added session to talk

--- talks/models.py
+++ talks/models.py
@@ -6,6 +6,20 @@ from base.models import TimeStampedModel
 from speakers.models import SpeakerProfile
 from talks.choices import TalkCategoryChoices
 
+PRESENTATION_FILES_UPLOAD_DIR = "presentation_files"
+
+
+class Session(TimeStampedModel):
+    """talk session model."""
+
+    type = models.CharField(
+        max_length=255, help_text="session type. eg. Keynote, lightning talk. etc"
+    )
+    duration = models.IntegerField(help_text="session duration in minutes")
+    talk = models.ForeignKey(
+        "Talks", on_delete=models.CASCADE, related_name="talk_sessions"
+    )
+
 
 class Talks(TimeStampedModel):
     """Talks model."""
@@ -17,6 +31,10 @@ class Talks(TimeStampedModel):
     )
     duration = models.IntegerField()
     category = models.CharField(max_length=100, choices=TalkCategoryChoices.choices)
+    presentation_files = models.FileField(
+        upload_to=PRESENTATION_FILES_UPLOAD_DIR, null=True
+    )
+
     # TODO: event = models.ForeignKey(
     #     "events.Event", on_delete=models.CASCADE, related_name="talk_event"
     # )

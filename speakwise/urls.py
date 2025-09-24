8ab7fef939de11ebdb66365ecb0de2430d4f8c87
ezi-code <ezrayendau2000@hotmail.com>
2025-09-24 00:37:00 +0000

    setup static files

--- speakwise/urls.py
+++ speakwise/urls.py
@@ -1,21 +1,7 @@
-"""URL configuration for speakwise project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+"""project urls."""
 
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import (
@@ -24,23 +10,29 @@ from drf_spectacular.views import (
     SpectacularSwaggerView,
 )
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-    path(
-        "api/docs/",
-        SpectacularSwaggerView.as_view(url_name="schema"),
-        name="swagger-ui",
-    ),
-    path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
-    path("api/", include("speakers.urls", namespace="speakers")),
-    path("api/", include("organizers.urls", namespace="organizers")),
-    path("api/", include("attendees.urls", namespace="attendees")),
-    path("api/", include("talks.urls", namespace="talks")),
-    path("api/", include("users.urls", namespace="users")),
-    path("api/", include("teams.urls", namespace="teams")),
-    path("api/", include("events.urls", namespace="events")),
-]
+urlpatterns = (
+    [
+        path("admin/", admin.site.urls),
+        path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+        path(
+            "api/docs/",
+            SpectacularSwaggerView.as_view(url_name="schema"),
+            name="swagger-ui",
+        ),
+        path(
+            "api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"
+        ),
+        path("api/", include("speakers.urls", namespace="speakers")),
+        path("api/", include("organizers.urls", namespace="organizers")),
+        path("api/", include("attendees.urls", namespace="attendees")),
+        path("api/", include("talks.urls", namespace="talks")),
+        path("api/", include("users.urls", namespace="users")),
+        path("api/", include("teams.urls", namespace="teams")),
+        path("api/", include("events.urls", namespace="events")),
+    ]
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)
 if settings.DEBUG:
     import debug_toolbar
 

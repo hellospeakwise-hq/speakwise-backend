dc60bead038c7c0934d9f5ad3f9e78bbad43d5f7
ezi-code <ezrayendau2000@hotmail.com>
2025-09-26 20:43:49 +0000

    work in progress
    # Conflicts:
    #	speakers/serializers.py

--- speakers/urls.py
+++ speakers/urls.py
@@ -17,4 +17,5 @@ urlpatterns = [
         views.SpeakerProfileRetrieveUpdateDestroyView.as_view(),
         name="speakers_retrieve_update_delete",
     ),
+    path('speakers/request-speaker/', views.RequestSpeakerView.as_view(), name='request_speaker'),
 ]

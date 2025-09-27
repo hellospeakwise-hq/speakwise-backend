45d2163b419a99224ff46aa8c2378c2beec09fac
ezi-code <ezrayendau2000@hotmail.com>
2025-09-27 08:50:53 +0000

    add nationality to user serializer and admin

--- users/admin.py
+++ users/admin.py
@@ -9,7 +9,15 @@ from users.models import User, UserRole
 class UserAdmin(admin.ModelAdmin):
     """user admin."""
 
-    fields = ["username", "email", "first_name", "last_name", "password", "role", "nationality"]
+    fields = [
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "password",
+        "role",
+        "nationality",
+    ]
     search_fields = ["nationality", "role", "email"]
     list_display = ["username", "email", "role"]
 

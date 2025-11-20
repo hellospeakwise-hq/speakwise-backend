c82a8051d32f896b21ffca6318976659f380519b
ezi-code <ezrayendau2000@hotmail.com>
2025-11-20 01:50:09 +0000

    run pre-commit

--- feedbacks/tests.py
+++ feedbacks/tests.py
@@ -2,11 +2,8 @@
 
 from django.contrib.auth import get_user_model
 from django.test import TestCase
-from rest_framework import status
-from rest_framework.test import APITestCase
 
 from feedbacks.models import Feedback
-from speakers.models import SpeakerProfile
 
 
 class TestFeedback(TestCase):

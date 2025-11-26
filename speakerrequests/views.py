f07d2c0f47c1a37f4b113449fdf77064cc54eb07
ezi-code <ezrayendau2000@hotmail.com>
2025-11-26 14:39:30 +0000

    run pre-commit

--- speakerrequests/views.py
+++ speakerrequests/views.py
@@ -7,7 +7,6 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from organizations.models import Organization
 from speakerrequests.choices import RequestStatusChoices
 from speakerrequests.models import SpeakerRequest
 from speakerrequests.serializers import SpeakerRequestSerializer

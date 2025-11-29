cc1569f150e5524aff8eb98a64b42946a70c13fc
ezi-code <ezrayendau2000@hotmail.com>
2025-11-29 01:10:54 +0000

    add speaker expereinces to admin

--- speakers/admin.py
+++ speakers/admin.py
@@ -2,10 +2,16 @@
 
 from django.contrib import admin
 
-from speakers.models import SpeakerProfile, SpeakerSkillTag, SpeakerSocialLinks
+from speakers.models import (
+    SpeakerExperiences,
+    SpeakerProfile,
+    SpeakerSkillTag,
+    SpeakerSocialLinks,
+)
 
 admin.site.register(SpeakerProfile)
 admin.site.register(SpeakerSocialLinks)
 admin.site.register(SpeakerSkillTag)
+admin.site.register(SpeakerExperiences)
 
 # Register your models here.

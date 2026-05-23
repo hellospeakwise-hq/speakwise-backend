654c1533cf998860e030b5936886f7b3f8c17785
Julius Boakye <bbjulius4@gmail.com>
2026-05-23 22:32:25 +0000

    fix: set console log level to INFO for staging visibility

--- speakwise/settings/production.py
+++ speakwise/settings/production.py
@@ -115,7 +115,7 @@ LOGGING = {
             "formatter": "verbose",
         },
         "console": {
-            "level": "ERROR",
+            "level": "INFO",
             "class": "logging.StreamHandler",
             "formatter": "verbose",
         },

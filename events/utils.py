57660536ba8e7ddabe365c9eb58eed75c6353bf4
Julius Boakye <bbjulius4@gmail.com>
2026-05-23 21:51:59 +0000

    Fix MultipleObjectsReturned error when user has multiple org memberships

--- events/utils.py
+++ events/utils.py
@@ -14,15 +14,15 @@ def create_event_payload(request):
     """Create event payload."""
     try:
         user = request.user
-        membership = OrganizationMembership.objects.get(user=user)
+        membership = OrganizationMembership.objects.filter(user=user).first()
+        if not membership:
+            raise Http404("User is not a member of any organization.")
         if membership.is_admins() or membership.is_organizers():
             organization = membership.organization
         else:
             raise Http404("User does not have permission to create an event.")
     except Organization.DoesNotExist as err:
         raise Http404("User does not belong to any organization.") from err
-    except OrganizationMembership.DoesNotExist as err:
-        raise Http404("User is not a member of the organization.") from err
 
     payload = request.data.copy()
     payload["organizer"] = organization.id

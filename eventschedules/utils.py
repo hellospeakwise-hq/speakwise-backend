5bba1335dc47a941aba358de4164470ee6203592
ezi-code <ezfrayendau2000@hotmail.com>
2026-07-11 00:58:18 +0000

    refactor event schedule payload creation

--- eventschedules/utils.py
+++ eventschedules/utils.py
@@ -5,17 +5,14 @@ from uuid import UUID
 from eventsessions.models import Session
 
 
-def create_event_schedule_payload(event: UUID) -> dict:
+def create_event_schedule_payload(event_id: UUID) -> dict:
     """Create an event schedule from payload."""
     schedule_sessions = []
-    sessions = Session.objects.filter(event=event)
+    sessions = Session.objects.filter(event=event_id)
     if sessions.count() == 0:
-        raise ValueError(f"No sessions found for event {event}")
+        raise ValueError(f"No sessions found for event {event_id}")
 
     for session in sessions:
         schedule_sessions.append(session.id)
 
-    return {
-        "event": event,
-        "sessions": schedule_sessions,
-    }
+    return dict(event=event_id, sessions=schedule_sessions)

69f4c4550988ca22a58af4be1ca95d91e5829941
Julius Boakye <julius.boakye@paicore.tech>
2026-07-30 22:21:57 +0000

    chore: update migration dependencies and refactor model field definitions

--- feedbacks/migrations/0005_alter_feedback_speaker.py
+++ feedbacks/migrations/0005_alter_feedback_speaker.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('feedbacks', '0004_convert_ids_to_uuid'),
+        ('feedbacks', '0005_feedback_event_eventfeedbackpreference'),
         ('speakers', '0016_alter_notification_is_read'),
     ]
 

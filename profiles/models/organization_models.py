1affc4cff73362407d6e2a4e9e3600d094bd8b29
ezi-code <ezfrayendau2000@hotmail.com>
2026-08-25 16:51:37 +0000

    add CPF model for organization to add open CFPs easily

--- profiles/models/organization_models.py
+++ profiles/models/organization_models.py
@@ -3,6 +3,7 @@
 import uuid
 
 from django.db import models
+from django.utils import timezone
 
 from base.models import TimeStampedModel
 
@@ -27,10 +28,36 @@ class OrganizationProfile(TimeStampedModel):
     contact_email = models.EmailField(
         blank=True, null=True, help_text="Contact email for the organization"
     )
-    website = models.URLField(
-        blank=True, null=True, help_text="Organization website URL"
+    cfps = models.ForeignKey(
+        "profiles.OrganizationCFP",
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="organization_cfp",
     )
 
     def __str__(self):
         """String representation of the organization profile."""
         return self.name
+
+    def get_open_cfps(self):
+        """Return all open CFPs for the organization."""
+        return self.cfp.filter(
+            open_at__lte=timezone.now(), close_at__gte=timezone.now()
+        )
+
+    def get_closed_cfps(self):
+        """Return all closed CFPs for the organization."""
+        return self.cfp.filter(close_at__lt=timezone.now())
+
+
+class OrganizationCFP(TimeStampedModel):
+    """Organization CFP model."""
+
+    url = models.URLField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    open_at = models.DateTimeField(blank=True, null=True)
+    close_at = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        """String representation of the organization CFP."""
+        return self.url
--- profiles/serializers/organization_serializers.py
+++ profiles/serializers/organization_serializers.py
@@ -2,14 +2,26 @@
 
 from rest_framework import serializers
 
-from profiles.models.organization_models import OrganizationProfile
+from profiles.models.organization_models import OrganizationCFP, OrganizationProfile
+
+
+class OrganizationCFPSerializer(serializers.ModelSerializer):
+    """Serializer for OrganizationCFP model."""
+
+    class Meta:
+        """Meta-options for OrganizationCFPSerializer."""
+
+        model = OrganizationCFP
+        fields = ["url", "description", "open_at", "close_at"]
 
 
 class OrganizationProfileSerializer(serializers.ModelSerializer):
     """Serializer for OrganizationProfile model."""
 
+    cfps = OrganizationCFPSerializer(many=True, required=False)
+
     class Meta:
-        """Meta options for OrganizationProfileSerializer."""
+        """Meta-options for OrganizationProfileSerializer."""
 
         model = OrganizationProfile
         fields = [
@@ -19,4 +31,5 @@ class OrganizationProfileSerializer(serializers.ModelSerializer):
             "website",
             "branding",
             "contact_email",
+            "cfps",
         ]
